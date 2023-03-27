74f370b97e00ebbb926b3c1c79dcac3da2e1970b
JoceyChan <joceychan331718@gmail.com>
2023-03-27 13:40:16 -0700

    Problem 1.4 Palindrome Premutations

---
+++ arrays/palindromePrem/Main.java
@@ -0,0 +1,12 @@
+package arrays.palindromePrem;
+
+public class Main {
+    
+}
+// Problem 1.4 Palindrome Premutations 
+// Given a function to check if it is a premutation of a palindrome. 
+// A palindrome is a word or phrase that is the same fowards and backwards. 
+// A premutation is a rearrangment of letters. The palindrome does not need 
+// to be limited to just dictionary words. 
+
+// I/P: Tact Coa || O/P: True (permutations: "taco cat", "atco cta", etc.)
