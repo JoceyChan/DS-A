00d1fb5d5a3c355fc9fd5529be3ccf86381da7e1
JoceyChan <joceychan331718@gmail.com>
2023-04-12 16:01:21 -0700

    leetcode

---
+++ leetcode/longestSubstring/Main.java
@@ -0,0 +1,19 @@
+package leetcode.longestSubstring;
+
+public class Main {
+    public static void main(String[] args){
+        String s = "abcabcbb";
+        String s2 = "bbbbb";
+        String s3 = "pwwkew";
+        char[] charArray = s.toCharArray();
+        int count = 0;
+        if(s2.length() <= 1){
+            System.out.println(s);
+        }
+        while(s.length() > 1){
+
+        }
+        
+    } 
+    
+}
