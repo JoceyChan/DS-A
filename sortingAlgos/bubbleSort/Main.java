31592e7ce83557e8d83d8da576412453c85ae921
JoceyChan <joceychan331718@gmail.com>
2023-04-12 16:10:34 -0700

    swap method

--- sortingAlgos/bubbleSort/Main.java
+++ sortingAlgos/bubbleSort/Main.java
@@ -2,6 +2,15 @@ package sortingAlgos.bubbleSort;
 
 public class Main {
     public static void main(String[] args){
-        
+
+    }
+    public static void swap(int[] array, int i, int j){ // swap method
+        if(i == j){
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
