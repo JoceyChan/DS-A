7456a5bd682b07c6c18a7a3734fc3165e784d8b0
JoceyChan <joceychan331718@gmail.com>
2023-04-12 16:11:45 -0700

    creating array in main method

--- sortingAlgos/bubbleSort/Main.java
+++ sortingAlgos/bubbleSort/Main.java
@@ -2,6 +2,7 @@ package sortingAlgos.bubbleSort;
 
 public class Main {
     public static void main(String[] args){
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
 
     }
     public static void swap(int[] array, int i, int j){ // swap method
