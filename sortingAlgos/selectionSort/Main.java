3effd9a6e3c8fc5433a5803dcf5872399807ecf4
JoceyChan <joceychan331718@gmail.com>
2023-04-12 17:02:06 -0700

    main method

--- sortingAlgos/selectionSort/Main.java
+++ sortingAlgos/selectionSort/Main.java
@@ -1,6 +1,13 @@
 package sortingAlgos.selectionSort;
 
 public class Main {
+    public static void main(String[] args){
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
+
+        for(int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+    }
     public static void swap(int[] array, int i, int j){
         if(i == j){
             return;
