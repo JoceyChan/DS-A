b2323c56b5583aad08b6675629faf5c03c970094
JoceyChan <joceychan331718@gmail.com>
2023-04-12 17:00:16 -0700

    swap method

---
+++ sortingAlgos/selectionSort/Main.java
@@ -0,0 +1,13 @@
+package sortingAlgos.selectionSort;
+
+public class Main {
+    public static void swap(int[] array, int i, int j){
+        if(i == j){
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
