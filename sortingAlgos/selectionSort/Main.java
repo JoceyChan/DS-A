a4bd630c301e3534f4f25862c05260043c72c2b1
JoceyChan <joceychan331718@gmail.com>
2023-04-12 17:12:22 -0700

    Solution for Selection Sort

--- sortingAlgos/selectionSort/Main.java
+++ sortingAlgos/selectionSort/Main.java
@@ -4,6 +4,18 @@ public class Main {
     public static void main(String[] args){
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
 
+        // The last unsorted index will be the last valid index 
+        for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){ //sorting partition by 1
+            int largest = 0;
+
+            for(int i = 1; i <= lastUnsortedIndex; i++){ // looking for the largest element
+                if(intArray[i] > intArray[largest]){
+                    largest = i;
+                }
+            }
+            swap(intArray, largest, lastUnsortedIndex);
+        } 
+
         for(int i = 0; i < intArray.length; i++){
             System.out.println(intArray[i]);
         }
