5c668f10ec02a2f24956fb9caf564d11fb8c6ac1
JoceyChan <joceychan331718@gmail.com>
2023-04-20 15:24:17 -0700

    Solved Brute Force, Time: O(n^2), Space: O(1)

--- leetcode/twoSum/Main.java
+++ leetcode/twoSum/Main.java
@@ -6,7 +6,27 @@ public class Main {
         int[] nums = {2, 7, 11, 15};
         int target = 9; 
 
-        arrayListSol(nums, target); 
+        bruteForce(nums, target); 
+        // arrayListSol(nums, target); 
+    }
+    // Time Complexity: O(n^2)
+    // Space Complexity: O(1)
+    public static void bruteForce(int[] nums, int target){
+        int[] res = new int[2];
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                int sum = nums[i] + nums[j];
+                if(sum == target && i != j){
+                    res[0] = i;
+                    res[1] = j;
+                    System.out.println(res[0]);
+                    System.out.println(res[1]);
+                    // return res;
+                }
+            }
+        }
+        // return null;
     }
 
     // Time Complexity: O(n^2)
