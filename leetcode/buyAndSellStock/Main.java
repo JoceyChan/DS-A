e75c5ea4c8ed1790a35a267ee496df04bcd9ffac
JoceyChan <joceychan331718@gmail.com>
2023-04-20 17:30:41 -0700

    main method

---
+++ leetcode/buyAndSellStock/Main.java
@@ -0,0 +1,9 @@
+package leetcode.buyAndSellStock;
+
+public class Main {
+    public static void main(String[] args){
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+
+        bruteForce(prices);
+    }
+}
