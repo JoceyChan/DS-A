bb7f1da3fd66799a175005c3154f6e8960bab80e
JoceyChan <joceychan331718@gmail.com>
2023-04-20 17:53:43 -0700

    Optimized Solution, Time: O(n), Space: O(1)

--- leetcode/buyAndSellStock/Main.java
+++ leetcode/buyAndSellStock/Main.java
@@ -4,12 +4,33 @@ public class Main {
     public static void main(String[] args){
         int[] prices = new int[]{7, 1, 5, 3, 6, 4};
 
-        bruteForce(prices);
+        // bruteForce(prices);
+        optimizedSolution(prices);
     }
 
     // Time Complexity: O(n)
     // Space Complexity: O(1)
     public static int bruteForce(int[] prices){
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            else{
+                int diff = prices[i] - minPrice;
+                // System.out.println(diff);
+                if(diff > maxProfit){
+                    maxProfit = diff;
+                }
+            }
+        }
+        System.out.println(maxProfit);
+        return maxProfit;
+    }
+
+    public static int optimizedSolution(int[] prices){
         int maxProfit = 0;
         int minPrice = Integer.MAX_VALUE;
 
